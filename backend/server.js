9af705ade6a62df1afaf21494e7576b4297fde96
Bruno Lujanović <brunolujanovic50@gmail.com>
2024-10-01 21:15:02 +0200

    routing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const synonymsRouter = require('./routes/synonyms');
 
 const app = express();
@@ -9,8 +10,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// API Routes
 app.use('/api/synonyms', synonymsRouter);
+
+// The "catchall" handler: for any request that doesn't match above,
+// send back the React app.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Start server
 app.listen(PORT, () => {
